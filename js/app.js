3d6583aa5138df638ffc5192cb7c69f81b684426
Luciano Angeleri <lcnang@gmail.com>
2023-04-16 20:20:40 -0300

    Se añadió la funcion para mostrar los datos de la persona creada

--- js/app.js
+++ js/app.js
@@ -1,36 +1,53 @@
 let btnCrear = document.getElementById("btnCrear")
 let btnBorrar = document.getElementById("btnBorrar")
 let btnGeneracion = document.getElementById("btnGeneracion")
-let btnDatos = document.getElementById("btnDatos")
+let btnMayor = document.getElementById("btnMayor")
+let contenedorCreacion = document.getElementById("contenedorCreacion")
 let formCreacion = document.querySelector("form")
 
 btnCrear.addEventListener("click",mostrarFormCreacion)
 btnCrear.addEventListener("click",borrarPersona)
 btnGeneracion.addEventListener("click",mostrarGeneracion)
-btnDatos.addEventListener("click",mostrarDatos)
+btnMayor.addEventListener("click",mostrarMayorEdad)
 formCreacion.addEventListener("submit",enviarForm)
 
 function mostrarFormCreacion(){
-    let contenedorCreacion = document.getElementById("contenedorCreacion")
     contenedorCreacion.classList.remove("d-none")
     btnCrear.classList.add("d-none")
 }
 function enviarForm(e) {
     e.preventDefault();
     btnGeneracion.classList.remove("d-none")
-    btnDatos.classList.remove("d-none")
+    btnMayor.classList.remove("d-none")
     btnBorrar.classList.remove("d-none")
+
+    let nombre = document.getElementById("inputNombre").value
+    let edad = document.getElementById("inputEdad").value
+    let dni = document.getElementById("inputDNI").value
+    let sexo = document.getElementById("inputSexo").value
+    let peso = document.getElementById("inputPeso").value
+    let altura = document.getElementById("inputAltura").value
+    let nacimiento = document.getElementById("inputNac").value
+
+    let contenedorDatos = document.getElementById("contenedorDatos")
+
+    let persona = new Persona (nombre,edad,dni,sexo,peso,altura,nacimiento)
+    
+    contenedorCreacion.classList.add("d-none")
     formCreacion.reset();
+    contenedorDatos.classList.remove("d-none")
+    persona.mostrarDatos();
+
 }
 
 function mostrarGeneracion() {
     
 }
-function mostrarDatos() {
+function mostrarMayorEdad() {
     
 }
 function borrarPersona(){
-
+    delete Persona
 }
 
 //Clase Persona
@@ -126,35 +143,36 @@ class Persona {
     
       mostrarDatos() {
         const datosPersona = document.createElement("ul")
-        datosPersona.classList.add("list-group","py-3","fs-3")
+        datosPersona.classList.add("list-group","py-3","fs-3","shadow")
 
         const nombreItemLista = document.createElement("li")
-        nombreItemLista.classList.add("list-group-item")
+        nombreItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
         nombreItemLista.textContent = `Nombre: ${this.nombre}`
         datosPersona.appendChild(nombreItemLista)      
         const edadItemLista = document.createElement("li")
-        edadItemLista.classList.add("list-group-item")
-        edadItemLista.textContent = `edad: ${this.edad}`
+        edadItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
+        edadItemLista.textContent = `Edad: ${this.edad} años`
         datosPersona.appendChild(edadItemLista)                             
         const dniItemLista = document.createElement("li")
-        dniItemLista.classList.add("list-group-item")
+        dniItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
         dniItemLista.textContent = `DNI: ${this.dni}`
         datosPersona.appendChild(dniItemLista)
         const sexoItemLista = document.createElement("li")
-        sexoItemLista.classList.add("list-group-item")
+        sexoItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
         sexoItemLista.textContent = `Sexo: ${this.sexo === "H" ? "Hombre" : "Mujer"}`
         datosPersona.appendChild(sexoItemLista)
         const pesoItemLista = document.createElement("li")
-        pesoItemLista.classList.add("list-group-item")
+        pesoItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
         pesoItemLista.textContent = `Peso: ${this.peso} kilogramos`
         datosPersona.appendChild(pesoItemLista)
         const alturaItemLista = document.createElement("li")
-        alturaItemLista.classList.add("list-group-item")
+        alturaItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
         alturaItemLista.textContent = `Altura: ${this.altura} centímetros`
         datosPersona.appendChild(alturaItemLista)
         const nacimientoItemLista = document.createElement("li")
-        nacimientoItemLista.classList.add("list-group-item")
+        nacimientoItemLista.classList.add("list-group-item","text-bg-dark","border-warning")
         nacimientoItemLista.textContent = `Año de nacimiento: ${this.anioNacimiento}`
         datosPersona.appendChild(nacimientoItemLista)
+        contenedorDatos.appendChild(datosPersona)
     }
 }
