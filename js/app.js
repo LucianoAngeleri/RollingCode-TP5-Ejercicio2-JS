8122f0034516e542432d4a5d21238c5d323a433d
Luciano Angeleri <lcnang@gmail.com>
2023-04-16 18:41:47 -0300

    Se agregó el boton para borrar la persona

--- js/app.js
+++ js/app.js
@@ -1,18 +1,26 @@
 let btnCrear = document.getElementById("btnCrear")
+let btnBorrar = document.getElementById("btnBorrar")
 let btnGeneracion = document.getElementById("btnGeneracion")
 let btnDatos = document.getElementById("btnDatos")
 let formCreacion = document.querySelector("form")
 
 btnCrear.addEventListener("click",mostrarFormCreacion)
+btnCrear.addEventListener("click",borrarPersona)
 btnGeneracion.addEventListener("click",mostrarGeneracion)
 btnDatos.addEventListener("click",mostrarDatos)
+formCreacion.addEventListener("submit",enviarForm)
 
 function mostrarFormCreacion(){
     let contenedorCreacion = document.getElementById("contenedorCreacion")
     contenedorCreacion.classList.remove("d-none")
+    btnCrear.classList.add("d-none")
+}
+function enviarForm(e) {
+    e.preventDefault();
     btnGeneracion.classList.remove("d-none")
     btnDatos.classList.remove("d-none")
-    btnCrear.classList.add("d-none")
+    btnBorrar.classList.remove("d-none")
+    formCreacion.reset();
 }
 
 function mostrarGeneracion() {
@@ -20,4 +28,7 @@ function mostrarGeneracion() {
 }
 function mostrarDatos() {
     
+}
+function borrarPersona(){
+
 }
